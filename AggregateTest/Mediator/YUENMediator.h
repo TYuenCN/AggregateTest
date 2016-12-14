54fa436937e4a714742d37a766adcd0a68891744
袁峥 <AugustYuen@yuanzhengdeMacBook-Pro.local>
2016-12-14 20:19:38 +0800

    Init Project

---
+++ AggregateTest/Mediator/YUENMediator.h
@@ -0,0 +1,18 @@
+//
+//  YUENMediator.h
+//  AggregateTest
+//
+//  Created by 袁峥 on 16/12/14.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+
+#ifndef YUENMediator_h
+#define YUENMediator_h
+#import <Foundation/Foundation.h>
+
+@interface YUENMediator : NSObject
+
+@end
+
+#endif
---
+++ AggregateTest/Mediator/YUENMediator.m
@@ -0,0 +1,13 @@
+//
+//  YUENMediator.m
+//  AggregateTest
+//
+//  Created by 袁峥 on 16/12/14.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "YUENMediator.h"
+
+@implementation YUENMediator
+
+@end
